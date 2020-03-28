476c33879d8b93aeca92180dfed5a12a290f42b9
Sarah Grabinski <sarah.grabinski@gmail.com>
2020-03-28 13:11:52 -0400

    Dictionary set

--- main.py
+++ main.py
@@ -2,17 +2,33 @@
 import csv
 
 # Declare variables
+votes = {}
+voter_ids = []
 results = {}
 
 # Read in CSV
+import_path = "election_data.csv"
+with open(import_path, newline='') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    header = next(csv_reader)
+
+    for row in csv_reader:
+        voter_ids.append(row[0])
+        votes.setdefault(row[2], []).append(row[0])
 
 # Calculate total number of votes
+total_votes = len(set(voter_ids))
 
 # List of candidates who received votes
+candidates = list(results.keys())
 
-# Percentage of votes won by each candidate
+# Total votes and percentage of votes won by each candidate
+for candidate in votes:
+    results.setdefault(candidate, {})
+    results[candidate].setdefault("Total Votes", len(votes[candidate]))
+    results[candidate].setdefault("Percentage of Votes", len(votes[candidate])/total_votes)
 
-# Total of votes won by each candidate
+print(results)
 
 # Winner of election based on popular vote
 
@@ -20,7 +36,7 @@ results = {}
 output = (
     f'\nElection Results\n'
     f'---------------------\n'
-    f'Total Votes: \n'
+    f'Total Votes: {total_votes}\n'
     f'---------------------\n'
     f'Name: % (total)\n'
     f'---------------------\n'
