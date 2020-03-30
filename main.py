221aa9b3c578fa5a1922c88644c84f352d5e06f0
Sarah Grabinski <sarah.grabinski@gmail.com>
2020-03-30 09:15:11 -0400

    Final, formatted

--- main.py
+++ main.py
@@ -14,33 +14,39 @@ with open(import_path, newline='') as csv_file:
     for row in csv_reader:
         votes.append(row[2])
 
-# Calculate total number of votes
-total_votes = len(votes)
+# Summarize results and export to file
+output_path = "results.txt"
+with open(output_path, "w") as txt_file:
 
-# List of candidates who received votes
-candidates = list(set(votes))
+    # Calculate total number of votes, print to terminal, and write to file
+    total_votes = len(votes)
+    totals_output = (
+        f'\nElection Results\n'
+        f'---------------------\n'
+        f'Total Votes: {total_votes:,.0f}\n'
+        f'---------------------\n'
+    )
+    print(totals_output, end="")
+    txt_file.write(totals_output)
 
-# Total votes won by each candidate
-for candidate in candidates:
-    results.setdefault(candidate, votes.count(candidate))
 
-# Winner of election based on popular vote
-winner = max(set(votes), key=votes.count)
+    # List of candidates who received votes
+    candidates = list(set(votes))
 
-# Print output to terminal and to .txt file
-output = (
-    f'\nElection Results\n'
-    f'---------------------\n'
-    f'Total Votes: {total_votes}\n'
-    f'---------------------\n'
-    f'Name: % (total)\n'
-    f'---------------------\n'
-    f'Winner: {winner}\n'
-    f'---------------------\n'
-)
+    # Total votes won by each candidate, print to terminal, and write to file
+    for candidate in candidates:
+        results.setdefault(candidate, votes.count(candidate))
+        vote_percent = results[candidate] / total_votes * 100
+        candidate_output = f'{candidate}: {vote_percent:.1f}% ({results[candidate]:,.0f})\n'
+        print(candidate_output, end="")
+        txt_file.write(candidate_output)
 
-print(output)
-
-export_path = "results.txt"
-with open(export_path, 'w') as txt_file:
-    txt_file.write(output)
+    # Winner of election based on popular vote, print to terminal, and write to file
+    winner = max(set(votes), key=votes.count)
+    winner_output = (
+        f'---------------------\n'
+        f'Winner: {winner}\n'
+        f'---------------------\n'
+    )
+    print(winner_output, end="")
+    txt_file.write(winner_output)
